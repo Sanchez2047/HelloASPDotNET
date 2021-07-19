e6342067eaae28e7081ae9b7b00c629f9fd5bb54
Mike <mike.sierra.7950@gmail.com>
2021-07-19 16:29:39 -0400

    init Commit

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloASPDotNET.Controllers
+{
+    [Route("/helloworld")]
+    public class HelloController : Controller
+    {
+        // GET: /<controrller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string html="<form method='post' action='/helloworld/'>"+
+                "<input type='text' name='name' />"+
+                "<input type='submit' value='Greet Me!' />"+
+                "</form>";
+            return Content(html, "text/html");
+        }
+
+        // GET: /hello/welcome
+        //[HttpGet]
+        //[Route("/helloworld/welcome/{name?}")]
+
+        [HttpGet("welcome/{name?}")]
+        [HttpPost]
+        public IActionResult Welcome(string name = "World")
+        {
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
+    }
+}
